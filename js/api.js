dc126f9b1412e8f672ba95edfa48a723a3a706a0
Jason Zhu <jasonzhu.dev@gmail.com>
2014-04-18 16:10:31 +0800

    basic function work now.

--- js/api.js
+++ js/api.js
@@ -1,25 +1,30 @@
+/* using ajax request text from remote server */
+function getText(url, callback) {
+    var request = new XMLHttpRequest();
+    request.open('GET', url);
+    request.onreadystatechange = function () {
+        if (request.readyState === 4 && request.status === 200) {
+            callback(request.responseText);
+        }
+    };
+    request.send(null);
+}
+
+/* put data in a table */
 function show(str, dst) {
     //console.log(data);
     var data = JSON.parse(str);
     var table = document.createElement('table');
+    table.style.padding = '20px';
     var tr;
     dst.innerHTML = '';
     for (var attr in data) {
         if (data.hasOwnProperty(attr)) {
             tr = document.createElement('tr');
             tr.innerHTML = '<td>' + attr + '</td>' +
-                '<td>' + (data[attr] || 'undefined') + '</td>';
+                '<td style="padding-left: 20%;"> ' + (data[attr] || 'undefined') + '</td>';
             table.appendChild(tr);
         }
     }
     dst.appendChild(table);
 }
-var srcUrl = 'https://freegeoip.net/json/';
-var button = document.querySelector('a.button');
-button.addEventListener('click', function() {
-    var ip = document.getElementsByTagName('input')[0].value;
-    getText(srcUrl + ip, function(data) {
-        var dst = document.querySelector('div.container.ip');
-        show(data, dst);
-    });
-});
